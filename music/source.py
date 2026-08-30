de09635ca928505ab0c4311eecb6b7071d117f34
Akash R <imakash566@gmail.com>
2026-08-30 15:16:15 +0530

    Refactor YouTube audio search to synchronous function

--- music/source.py
+++ music/source.py
@@ -1,20 +1,38 @@
+import asyncio
 import yt_dlp
 
 
+def _search_youtube(query):
+    options = {
+        "format": "bestaudio/best",
+        "quiet": True,
+        "no_warnings": True,
+        "default_search": "ytsearch1",
+        "noplaylist": True,
+    }
+
+    with yt_dlp.YoutubeDL(options) as ydl:
+        info = ydl.extract_info(query, download=False)
+
+    if not info:
+        raise Exception("No result found")
+
+    if "entries" in info:
+        info = info["entries"][0]
+
+    return {
+        "title": info.get("title", "Unknown"),
+        "url": info["url"],
+        "duration": info.get("duration", 0),
+        "thumbnail": info.get("thumbnail"),
+    }
+
+
 async def get_audio(query):
-    loop = __import__("asyncio").get_running_loop()
+    loop = asyncio.get_running_loop()
 
-    def search():
-        opts = {
-            "format": "bestaudio/best",
-            "quiet": True,
-            "default_search": "ytsearch",
-            "noplaylist": True,
-        }
-
-        with yt_dlp.YoutubeDL(opts) as ydl:
-            info = ydl.extract_info(query, download=False)
-
-        return info["entries"][0]["url"]
-
-    return await loop.run_in_executor(None, search)
+    return await loop.run_in_executor(
+        None,
+        _search_youtube,
+        query,
+    )
