06d89f2708bc335960544e95ce75bd4b9fc48062
Akash R <imakash566@gmail.com>
2026-08-30 15:08:57 +0530

    Update queue.py

--- music/queue.py
+++ music/queue.py
@@ -1,48 +1,25 @@
 class MusicQueue:
 
     def __init__(self):
-        self.queues = {}
+        self.data = {}
 
-    def _get(self, chat_id):
-        return self.queues.setdefault(chat_id, [])
+    def get(self, chat_id):
+        return self.data.setdefault(chat_id, [])
 
     def add(self, chat_id, song):
-        queue = self._get(chat_id)
+        queue = self.get(chat_id)
         queue.append(song)
         return len(queue)
 
-    def pop(self, chat_id):
-        queue = self._get(chat_id)
-
-        if not queue:
-            return None
-
-        return queue.pop(0)
-
-    def peek(self, chat_id):
-        queue = self._get(chat_id)
-
-        if not queue:
-            return None
-
-        return queue[0]
-
-    def get_all(self, chat_id):
-        return self._get(chat_id)
+    def next(self, chat_id):
+        queue = self.get(chat_id)
+        return queue.pop(0) if queue else None
 
     def clear(self, chat_id):
-        self.queues[chat_id] = []
+        self.data[chat_id] = []
 
-    def remove(self, chat_id, index):
-        queue = self._get(chat_id)
-
-        if index < 1 or index > len(queue):
-            return None
-
-        return queue.pop(index - 1)
-
-    def size(self, chat_id):
-        return len(self._get(chat_id))
+    def all(self, chat_id):
+        return self.get(chat_id)
 
 
-music_queue = MusicQueue()
+queue = MusicQueue()
