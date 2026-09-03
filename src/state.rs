dc38edea055a7623fdb5bba8f58900d3e5c895e9
Akash R <imakash566@gmail.com>
2026-09-03 15:05:05 +0530

    Update state.rs

--- src/state.rs
+++ src/state.rs
@@ -1,10 +1,34 @@
+use std::{
+    collections::{HashMap, VecDeque},
+    sync::Arc,
+};
 
-use std::sync::Arc;
+use tokio::sync::Mutex;
 use tgcalls::Calls;
+
+#[derive(Clone)]
+pub struct QueueItem {
+    pub query: String,
+}
+
+pub struct ChatQueue {
+    pub current: Option<QueueItem>,
+    pub queue: VecDeque<QueueItem>,
+}
+
+impl ChatQueue {
+    pub fn new() -> Self {
+        Self {
+            current: None,
+            queue: VecDeque::new(),
+        }
+    }
+}
 
 #[derive(Clone)]
 pub struct AppState {
     pub calls: Arc<Calls>,
     pub worker_secret: String,
-}
 
+    pub queues: Arc<Mutex<HashMap<i64, ChatQueue>>>,
+}
